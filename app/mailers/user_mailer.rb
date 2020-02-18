38c2e70d73d377a2c90a0c68b1420addd768fa90
Kjell Morgenstern <kjell@gmx.de>
2020-02-18 10:23:50 +0100

    Configure mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,21 +3,21 @@ class UserMailer < ActionMailer::Base
 
   def activation_needed_email(user)
     @user = user
-    @url  = "http://0.0.0.0:3000/users/#{user.activation_code}/activate"
+    @url  = activate_user_url(user.activation_token)
     mail(to: user.email,
          subject: 'Welcome to My Awesome Site')
   end
 
   def activation_success_email(user)
     @user = user
-    @url  = 'http://0.0.0.0:3000/login'
+    @url  = login_url
     mail(to: user.email,
          subject: 'Your account is now activated')
   end
 
   def reset_password_email(user)
     @user = user
-    @url  = "http://0.0.0.0:3000/password_resets/#{user.reset_password_token}/edit"
+    @url  = edit_password_reset_url(@user.reset_password_token)
     mail(to: user.email,
          subject: 'Your password reset request')
   end
