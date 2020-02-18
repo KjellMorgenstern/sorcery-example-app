c17c8e895d4cf796dd16aae4d7c1e04d1e179510
Kjell Morgenstern <kjell@gmx.de>
2020-02-18 11:25:50 +0100

    set url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,13 +86,18 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
   
+  host = 'sorcery-sso.herokuapp.com'
+
   config.action_mailer.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'sorcery-sso.herokuapp.com',
+    :domain => host,
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
     :enable_starttls_auto => true
   }
+
+  config.action_controller.default_url_options = {:host => host}
+  config.action_mailer.default_url_options = {:host => host}
 end
